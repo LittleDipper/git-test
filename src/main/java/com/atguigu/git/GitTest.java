74bc918860e9173caa368937c26b3196a0104af4
hz21076971 <2435049674@qq.com>
2022-03-06 22:51:25 +0800

    first commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("gitee test!");
         System.out.println("gitee pull test!");
         System.out.println("tttttt");
+        System.out.println("HEHE");
     }
 }
